249fd4f4bd551e36ff61a9f2cab2471090227833
Bartlomiej <bartek21122006@gmail.com>
2024-09-21 18:56:55 +0200

    made properties file public, added maven compiler plugin

--- src/main/java/pl/bartlomiej/keycloakidmservice/internal/KeycloakIDMServiceProperties.java
+++ src/main/java/pl/bartlomiej/keycloakidmservice/internal/KeycloakIDMServiceProperties.java
@@ -3,7 +3,7 @@ package pl.bartlomiej.keycloakidmservice.internal;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "keycloak-idm-service")
-record KeycloakIDMServiceProperties(
+public record KeycloakIDMServiceProperties(
         Boolean enabled,
         String serverUrl,
         String realmName,
