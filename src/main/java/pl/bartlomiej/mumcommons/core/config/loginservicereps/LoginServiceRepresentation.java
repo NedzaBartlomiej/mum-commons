3eab18eae8969e42261950ab27c9a895ba7b5144
Bartlomiej <bartek21122006@gmail.com>
2025-01-12 15:54:14 +0100

    extracted  login services feature to an individual library

--- src/main/java/pl/bartlomiej/mumcommons/core/config/loginservicereps/LoginServiceRepresentation.java
+++
@@ -1,4 +0,0 @@
-package pl.bartlomiej.mumcommons.core.config.loginservicereps;
-
-public record LoginServiceRepresentation(String hostname, int port, String loginResourceIdentifier, String clientId) {
-}
